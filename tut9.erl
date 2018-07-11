018eca1ada4ea6118036169bb992885432d02236
Deniss Bohanovs <deniss@fonix.com>
2018-07-11 12:51:57 +0100

    Add Update record object

--- tut9.erl
+++ tut9.erl
@@ -1,8 +1,14 @@
 -module(tut9).
--export([start/0]).
+-export([start/0, update/0]).
 -record(person, {name = "", id}).
 
 start() ->
    P = #person{name="John",id=1},
    io:fwrite("~p~n",[P#person.id]),
    io:fwrite("~p~n",[P#person.name]).
+
+update() ->
+   P = #person{name = "John",id = 1},
+   P1 = P#person{name = "Dan"},
+   io:fwrite("~p~n",[P1#person.id]),
+   io:fwrite("~p~n",[P1#person.name]).
