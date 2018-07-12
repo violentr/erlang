f0c2a9181305f0cd51f5e333908a186b92ddcc23
Deniss Bohanovs <deniss@fonix.com>
2018-07-12 15:24:26 +0100

    Add more example of Recursion

--- tut26.erl
+++ tut26.erl
@@ -32,5 +32,27 @@ beach(Temperature) ->
         _ ->
             'avoid beach'
     end.
+
+fac(N) when N == 0 -> 1;
+fac(N) when N > 0  -> N*fac(N-1).
+
+duplicate(0,_) ->
+[];
+duplicate(N,Term) when N > 0 ->
+io:fwrite("TERM is ~p~n",[Term]),
+[Term|duplicate(N-1,Term)].
+
+partition(_,[], Smaller, Larger) -> {Smaller, Larger};
+partition(Pivot, [H|T], Smaller, Larger) ->
+    if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+       H >  Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+    end.
+
+quicksort([]) -> [];
+quicksort([Pivot|Rest]) ->
+    {Smaller, Larger} = partition(Pivot,Rest,[],[]),
+    quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
 start() ->
-  beach({}).
+  quicksort([10,12,3,4,9]).
+  %duplicate(4, "hello).
